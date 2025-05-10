bb609d323db9d756394669f8e39bc7c37f2fd184
Priyanshu Singh <singhpriyanshu073@gmail.com>
2025-05-11 01:20:02 +0530

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -123,8 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/images/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
