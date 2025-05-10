6a3bb4d626afc0ecd529d04c20ccc73fa966f81d
Priyanshu Singh <singhpriyanshu073@gmail.com>
2025-05-10 23:44:43 +0530

    versel commit

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'grbaqxf3&*^rohdhgtp01aq%3d!1&%d8pga^@a!p_15y367o@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
