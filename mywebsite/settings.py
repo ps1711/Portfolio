72c85db7743346928b2c0db78ed35942eaca8e31
Priyanshu Singh <singhpriyanshu073@gmail.com>
2025-05-11 01:28:34 +0530

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'mywebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "staticfiles"
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
