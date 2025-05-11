526bc3bb27c0453f3799a0f7aec7381de53e894e
Priyanshu Singh <singhpriyanshu073@gmail.com>
2025-05-11 14:49:24 +0530

    .

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'grbaqxf3&*^rohdhgtp01aq%3d!1&%d8pga^@a!p_15y367o@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
