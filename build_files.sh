c875669b8f009ba654cee2286809f3d3544f3cca
Priyanshu Singh <singhpriyanshu073@gmail.com>
2025-05-11 22:57:23 +0530

    .

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-PYTHON_EXECUTABLE=$(which python3) # Or just 'python' depending on the environment
+PYTHON_EXECUTABLE=$(which python3)
 
 if [ -n "$PYTHON_EXECUTABLE" ]; then
   echo "Using Python executable: $PYTHON_EXECUTABLE"
   "$PYTHON_EXECUTABLE" -m pip install -r requirements.txt
+  # Explicitly install Django again
+  "$PYTHON_EXECUTABLE" -m pip install Django
   "$PYTHON_EXECUTABLE" manage.py collectstatic --noinput
 else
   echo "Error: Python executable not found in PATH."
