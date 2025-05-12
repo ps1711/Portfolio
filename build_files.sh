006d3b3cf26e99a6fa81c448923afc3ae9d26b91
Priyanshu Singh <singhpriyanshu073@gmail.com>
2025-05-12 14:06:28 +0530

    ..

--- build_files.sh
+++ build_files.sh
@@ -7,6 +7,7 @@ if [ -n "$PYTHON_EXECUTABLE" ]; then
   "$PYTHON_EXECUTABLE" -m pip install --upgrade -r requirements.txt
   # Explicitly install Django again (with upgrade)
   "$PYTHON_EXECUTABLE" -m pip install --upgrade Django
+  "$PYTHON_EXECUTABLE" -m pip install --upgrade pip
   # Capture output and check for errors
   echo "Running collectstatic..."
   "$PYTHON_EXECUTABLE" manage.py collectstatic --noinput
